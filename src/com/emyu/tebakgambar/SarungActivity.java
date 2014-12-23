d62aa4e0ee77f0f13185569f2b8c03df139377da
emiliamuchtar <emiliamuchtar18@gmail.com>
2014-12-23 14:32:59 +0700

    tugas

---
+++ src/com/emyu/tebakgambar/SarungActivity.java
@@ -0,0 +1,55 @@
+package com.emyu.tebakgambar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SarungActivity extends Activity {
+	EditText jawabanTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sarung);
+		jawabanTxt = (EditText)findViewById(R.id.editText1);
+		jawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sarung, menu);
+		return true;
+	}
+	public void cekJawaban(View v){
+		String jawabanBenar = "SARUNG HP";
+		String jawabanUser = jawabanTxt.getText().toString();
+		if(jawabanUser.equals(jawabanBenar)){
+			//pesan Toast
+			Toast.makeText(getApplicationContext(), "JAWABAN TEPAT", Toast.LENGTH_LONG).show();
+			Intent i = new Intent(getApplicationContext(), SateActivity.class);
+			startActivity(i);
+		}
+
+	else {
+		//pesan toast
+		Toast.makeText(getApplicationContext(), "JAWABAN MASIH SALAH", Toast.LENGTH_LONG).show();
+	}
+}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
